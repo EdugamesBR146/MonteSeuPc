0b3900493c4a1e84ab382454b0dd1d40bb5c46dd
EdugamesBR146 <eduardomarcoslopessilva@gmail.com>
2024-08-31 13:50:34 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+const Buttons = document.querySelectorAll('.Sidebar button');
+
 let Active = false;
+
+function ReturnDesiredElement(father, type) {
+    for (i = 0; i < father.children.length; i++) {
+        if (father.children[i].tagName == type) {
+            return father.children[i];
+        }
+    }
+}
 
 function DropDown() {
     let Title = document.getElementById("Title");
@@ -10,9 +20,7 @@ function DropDown() {
     setTimeout(() => {
         for (let i = 0; i < TopBar.children.length; i++){
             if (TopBar.children[i].tagName == "BUTTON") {
-                let ArrowImage = document.getElementById("ArrowImage");
-
-                ArrowImage.style.opacity = 1;
+                document.getElementById("ArrowImage").style.opacity = 1;
             } else {
                 TopBar.children[i].style.opacity = 1;
             }
@@ -21,14 +29,38 @@ function DropDown() {
 }
 
 function NavOpen() {
+    let SideBar = document.getElementById("SideBar");
+   
     switch(Active) {
         case true:
-            
+            SideBar.style.width = "0px";
+            break;
         case false:
-
+            SideBar.style.width = "320px";
+            break;
     }
 
     Active = !Active
+}
+
+function OnMouseEnter(button) {
+    let Span = ReturnDesiredElement(button, "SPAN")
+
+    if (Span) {
+        button.style["border-color"] = "white";
+        Span.classList.add("Active");
+        Span.style.color = "white";
+    }
+}
+
+function OnMouseLeave(button) {
+    let Span = ReturnDesiredElement(button, "SPAN")
+
+    if (Span) {
+        button.style["border-color"] = "grey";
+        Span.classList.remove("Active");
+        Span.style.color = "grey";
+    }
 }
 
 window.onload = DropDown;
